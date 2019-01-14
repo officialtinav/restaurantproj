d03bd1c9045a599f5f4326a59dd8239d4601237b
tina <34047417+officialtinav@users.noreply.github.com>
2019-01-14 12:14:34 -1000

    Delete xml.js

--- xml.js
+++
@@ -1,8 +0,0 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadtstatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var mydata = JSON.parse(this.responseText);
-  }
-};
-xmlhttp.open("GET","jtest.json",true);
-xmlhttp.send();
